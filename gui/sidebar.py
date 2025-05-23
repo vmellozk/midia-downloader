1a37bcd64bdab45c020a3cac07a17417695e2c35
Victor Mello <contato.devictormello@gmail.com>
2025-05-23 17:11:23 -0300

    adicionando classe para cada botão da side bar, assim como mudando estrutura dos botões para poder rastrear o sinal do click do botão para trocar a página do aplicativo

--- gui/sidebar.py
+++ gui/sidebar.py
@@ -1,15 +1,36 @@
 from PySide6.QtWidgets import (
-    QWidget, QHBoxLayout, QPushButton, QLabel
+    QWidget, QHBoxLayout, QPushButton, QLabel, QVBoxLayout
 )
+from PySide6.QtCore import Signal
 
 class Sidebar(QWidget):
+    change_scr = Signal(str)
+    
     def __init__(self, parent=None):
         super().__init__(parent)
         layout = QHBoxLayout()
         layout.addStretch()
-        layout.addWidget(QPushButton("🏠"))
-        layout.addWidget(QPushButton("⬇️"))
-        layout.addWidget(QPushButton("♻️"))
-        layout.addWidget(QPushButton("⚙️"))
+        home_btn = QPushButton("🏠")
+        settings_btn = QPushButton("⚙️")
+        layout.addWidget(home_btn)
+        layout.addWidget(settings_btn)
         layout.addStretch()
+        self.setLayout(layout)
+        
+        # Conetando ao Signal de cada botão
+        home_btn.clicked.connect(lambda: self.change_scr.emit("home"))
+        settings_btn.clicked.connect(lambda: self.change_scr.emit("settings"))
+
+class HomeScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel())
+        self.setLayout(layout)
+        
+class SettingsScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("Configurações"))
         self.setLayout(layout)
